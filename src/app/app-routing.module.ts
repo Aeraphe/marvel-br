a764179817e49901dfcc3430b6ea5cc61fd21ddd
Aeraphe <alberto.aeraph@gmail.com>
2022-02-07 05:36:09 -0300

    chore: add items to app ngmodule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './layout/default/default.component';
+import { CharacterDetailsComponent } from './pages/character-details/character-details.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DefaultComponent,
+    children: [{ path: '', component: HomeComponent },{path:'character-details/:id',component:CharacterDetailsComponent}],
+    
+  },
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultModule } from './layout/default/default.module';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { SharedModule } from './shared/shared.module';
+import { CharacterDetailsComponent } from './pages/character-details/character-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    CharacterDetailsComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DefaultModule,
+    HttpClientModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
