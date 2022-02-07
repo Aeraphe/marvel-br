46be1fe01b1b8bcc3a599de9a6a556a21903d633
Aeraphe <alberto.aeraph@gmail.com>
2022-02-07 05:35:22 -0300

    feat(components): create character-details component

---
+++ src/app/pages/character-details/character-details.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { map, pipe } from 'rxjs';
+import { CharacterService } from 'src/app/shared/services/character.service';
+
+@Component({
+  selector: 'app-character-details',
+  templateUrl: './character-details.component.html',
+  styleUrls: ['./character-details.component.css'],
+})
+export class CharacterDetailsComponent implements OnInit {
+  characterInfo: any = {};
+  characterThumbnail: any = {};
+
+  constructor(
+    private actRoute: ActivatedRoute,
+    private http: CharacterService
+  ) {}
+
+  ngOnInit(): void {
+    this.actRoute.paramMap.subscribe((p: ParamMap) => {
+      console.log();
+      let id = p.get('id');
+      if (id != null) {
+        this.getCharacterDetailsHandler(+id);
+      }
+    });
+  }
+
+  getCharacterDetailsHandler(id: number) {
+    console.log(id);
+    let resp$ = this.http.getCharacterDetails(id).pipe(
+      map((character) => {
+        return character.data.results;
+      })
+    );
+
+    resp$.subscribe((resp) => {
+      if (resp.length > 0) {
+        this.characterInfo = resp[0];
+        
+        this.characterThumbnail = { ...this.characterInfo.thumbnail };
+        console.log( this.characterInfo);
+      }
+    });
+  }
+}
