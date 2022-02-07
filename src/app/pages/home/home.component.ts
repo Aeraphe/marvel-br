4737fd433fdb88482b2991c6e10557b516be636c
Aeraphe <alberto.aeraph@gmail.com>
2022-02-07 05:28:40 -0300

    feat(components): create home component

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { CharacterService } from '../../shared/services/character.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  characters$: Observable<any> = new Observable<any>();
+
+  constructor(private http: CharacterService,private router:Router) {}
+
+  ngOnInit(): void {}
+
+  onChangeInput(searchText: string) {
+    this.characters$ = this.http.getCharacters(searchText).pipe(
+      map((item) => {
+        console.log(item.data.results);
+        return item.data.results;
+      })
+    );
+  }
+
+  showCharacterDetails(id:number){
+
+    this.router.navigate(['/character-details/' +id]) 
+    console.log(id,"show");
+  }
+
+}
