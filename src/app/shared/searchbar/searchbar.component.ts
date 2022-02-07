316c4321f2263339fcb01e19ee9b27946f83c641
Aeraphe <alberto.aeraph@gmail.com>
2022-02-07 05:33:52 -0300

    feat(components): create shared searchbar component

---
+++ src/app/shared/searchbar/searchbar.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-searchbar',
+  templateUrl: './searchbar.component.html',
+  styleUrls: ['./searchbar.component.css'],
+})
+export class SearchbarComponent implements OnInit {
+  @Output() onChange: EventEmitter<string> = new EventEmitter<string>();
+  
+  searchText =  ""
+
+
+  constructor() {}
+
+
+  ngOnInit(): void {}
+
+  onChangeHandler(evt:any){
+    console.log(evt.target.value)
+    this.onChange.emit(evt.target.value)
+  }
+
+
+}
