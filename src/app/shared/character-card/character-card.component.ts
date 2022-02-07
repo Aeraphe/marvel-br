350a1cbce31a7ba7184220bd5b62c649d9406a3d
Aeraphe <alberto.aeraph@gmail.com>
2022-02-07 05:33:23 -0300

    feat(components): create shared character-card component

---
+++ src/app/shared/character-card/character-card.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-shared-character-card',
+  templateUrl: './character-card.component.html',
+  styleUrls: ['./character-card.component.css'],
+})
+export class CharacterCardComponent implements OnInit {
+  @Input() character: any;
+  imageSrc: string = '';
+  characterName = '';
+  id:number =  0;
+
+  @Output() onClick:EventEmitter<number> = new EventEmitter<number>()
+
+  constructor() {}
+
+  ngOnInit(): void {
+    if (this.character) {
+      this.id = this.character.id
+      let thumbnail = { ...this.character.thumbnail };
+      this.characterName = this.character.name;
+      this.imageSrc = thumbnail.path + '.' + thumbnail.extension;
+    }
+  }
+
+  onClickHandler(){
+    this.onClick.emit(this.id)
+  }
+}
