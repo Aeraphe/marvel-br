51f7dcc7b06443085c46d7cd2b2c8668cb4e22e0
Aeraphe <alberto.aeraph@gmail.com>
2022-02-08 13:45:38 -0300

    fix(components): move homecomponent to page module

--- src/app/layout/default/default.module.ts
+++ src/app/layout/default/default.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DefaultComponent } from './default.component';
-import { HomeComponent } from 'src/app/pages/home/home.component';
+
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -9,18 +9,14 @@ import { MobileMenuComponent } from './mobile-menu/mobile-menu.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { DesktopMenuComponent } from './desktop-menu/desktop-menu.component';
 
-
 @NgModule({
   declarations: [
     DefaultComponent,
-    HomeComponent,
     HeaderComponent,
     FooterComponent,
     MobileMenuComponent,
     DesktopMenuComponent,
-    
-    
   ],
-  imports: [CommonModule, RouterModule,SharedModule],
+  imports: [CommonModule, RouterModule, SharedModule],
 })
 export class DefaultModule {}
