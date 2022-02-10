04d8ee7ffff37c53613e1cdf58c9b1dd350b6c11
Aeraphe <alberto.aeraph@gmail.com>
2022-02-10 17:19:07 -0300

    feat(menu-items): add new items on menu list

--- src/app/layout/default/menuItems.ts
+++ src/app/layout/default/menuItems.ts
@@ -1,1 +1,8 @@
-export const   menuItem =['Home','Search','Comics','Movies'];
+export const menuItem = [
+  { route: '/home', name: 'Home' },
+  { route: 'characters', name: 'Search' },
+  { route: '/comics', name: 'Comics' },
+  { route: '/movies', name: 'Movies' },
+  { route: '/admin', name: 'Admin' },
+
+];
