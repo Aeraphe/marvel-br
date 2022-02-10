a04f390bf28b49c5240f046895121f32094f924a
Aeraphe <alberto.aeraph@gmail.com>
2022-02-10 16:44:17 -0300

    feat(routes): add admin page component route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 import { DefaultComponent } from './layout/default/default.component';
+import { AdminComponent } from './pages/admin/admin.component';
 import { CharacterDetailsComponent } from './pages/character-details/character-details.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+
 
 const routes: Routes = [
   {
@@ -12,8 +15,13 @@ const routes: Routes = [
     component: DefaultComponent,
     children: [
       { path: '', component: HomeComponent },
+      { path: 'home', component: HomeComponent },
+      { path: 'characters', component: HomeComponent },
+      { path: 'comics', component: HomeComponent },
+      { path: 'movies', component: HomeComponent },
       { path: 'character-details/:id', component: CharacterDetailsComponent },
       { path: 'login', component: LoginComponent },
+      {path:'admin', component:AdminComponent, canActivate:[AuthGuard]}
     ],
   },
   { path: '', redirectTo: '', pathMatch: 'full' },
