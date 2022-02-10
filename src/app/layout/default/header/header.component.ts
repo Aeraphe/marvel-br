99b08221084ddf72fe317a5f801fa337ede998b0
Aeraphe <alberto.aeraph@gmail.com>
2022-02-10 17:48:13 -0300

    feat(components): add check if user login on header nav

--- src/app/layout/default/header/header.component.ts
+++ src/app/layout/default/header/header.component.ts
@@ -1,25 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthenticateService } from 'src/app/shared/services/authenticate.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  navState = false;
+  isLogging$: Observable<string>;
+  menuItemName = 'Login';
 
-  navState =  false
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authservice: AuthenticateService) {
+    this.isLogging$ = this.authservice.isLoggedIn();
+    this.isLogging$.subscribe((is) => {
+      if (is === 'true') {
+        this.menuItemName = 'Logout';
+      } else {
+        this.menuItemName = 'Login';
+      }
+    });
   }
 
-  openNav(){
+  ngOnInit(): void {}
+
+  openNav() {
     this.navState = true;
-
   }
 
-  closeNav(state:any){
+  closeNav(state: any) {
     this.navState = state;
   }
-
 }
