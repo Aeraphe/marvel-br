336db9b264a8709a301337e9d5609e41a07af263
Aeraphe <alberto.aeraph@gmail.com>
2022-02-10 12:53:49 -0300

    feat(components): add router navigate  to login to home page dest

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router) { }
 
   ngOnInit(): void {
   }
 
 
-  onSubmit(){}
+  onSubmit(){
+    this.router.navigate(['/']);
+    console.log("login")
+  }
 
 }
