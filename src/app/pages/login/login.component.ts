c6270ddd6010a3fe09591c99d9f13502ebffa9cd
Aeraphe <alberto.aeraph@gmail.com>
2022-02-10 17:23:46 -0300

    feat(components): add authenticate service in login comp.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,22 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthenticateService } from 'src/app/shared/services/authenticate.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private authService: AuthenticateService
+  ) {}
 
-  constructor( private router: Router) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit() {
+    this.authService.login().subscribe((user) => {
+      console.log(user);
+    });
+    this.router.navigate(['/admin']);
+    console.log('login');
   }
-
-
-  onSubmit(){
-    this.router.navigate(['/']);
-    console.log("login")
-  }
-
 }
